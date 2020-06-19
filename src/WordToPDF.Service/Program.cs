260de45fd0e47f611ab940f16872afd2170662e7
Michael Terry <mterry@sancsoft.com>
2020-06-18 23:13:43 -0400

    Started on the service interface

---
+++ src/WordToPDF.Service/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Serilog;
+using Topshelf;
+
+namespace WordToPDF.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("appname", Assembly.GetEntryAssembly().GetName().Name)
+                .CreateLogger();
+
+            AssemblyName assemblyName = Assembly.GetEntryAssembly().GetName();
+            Log.Information(assemblyName.Name + " Version " + assemblyName.Version.ToString());
+            Log.Information("Copyright (c) 2020 - Sanctuary Software Studio, Inc.");
+
+            HostFactory.Run(x =>
+            {
+                x.Service<WordToPDFService>(s =>
+                {
+                    s.ConstructUsing(name => new WordToPDFService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("WordToPDF Service");
+                x.SetDisplayName("WordToPDF");
+                x.SetServiceName("WordToPDF");
+                x.UseSerilog();
+            });
+
+        }
+    }
+}
---
+++ src/WordToPDF.Service/WordToPDFService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using Serilog;
+
+namespace WordToPDF.Service
+{
+    public class WordToPDFService
+    {
+        protected System.Timers.Timer _processTimer;
+        protected bool _processLock;
+
+        public WordToPDFService()
+        {
+            Log.Debug("Construction of WordToPDFService");
+        }
+
+        public void Start()
+        {
+            Log.Warning($"Starting {Assembly.GetEntryAssembly().GetName()} on {Environment.MachineName}");
+            _processLock = false;
+            _processTimer = new System.Timers.Timer(10000) { AutoReset = true };
+            _processTimer.Elapsed += (sender, eventArgs) => ProcessDocuments();
+            _processTimer.Start();
+        }
+
+        public void Stop()
+        {
+            Log.Warning($"Stopping {Assembly.GetEntryAssembly().GetName()} on {Environment.MachineName}");
+            _processTimer.Stop();
+            _processTimer = null;
+        }
+
+        public void ProcessDocuments()
+        {
+            if(_processLock)
+            {
+                Log.Debug("Re-entry into service process, still busy.");
+                return;
+            }
+
+            _processLock = true;
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Log.Error("Unhandled exception in ProcessDocuments {0}", e);
+            }
+            _processLock = false;
+        }
+    }
+}
